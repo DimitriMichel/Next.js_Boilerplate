811602df8d38995ca0e342913e106b5dd67182b0
Dimitri PL <dimitrimachines@gmail.com>
2020-02-22 16:54:51 -0500

    Initial Commit

---
+++ components/Head.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import Head from "next/head";
+
+function IndexPage() {
+    return (
+        <Fragment>
+            <Head>
+                <title>Enter Your Title Here</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+        </Fragment>
+    );
+}
+
+export default IndexPage;
---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+// This default export is required in a new `pages/_app.js` file.
+// This page is used for Global SASS & CSS Styling
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
